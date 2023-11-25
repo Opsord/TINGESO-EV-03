57585bf9b7224295bc4ab67c1173fb95c23c0ee2
Andres Zelaya <andres.zelaya.d@hotmail.com>
2023-11-25 20:44:12 -0300

    Project initialization

---
+++ AsignamaxApp/asignamax-backend-service/src/main/java/fing/asignamax3000/Asignamax3000Application.java
@@ -0,0 +1,13 @@
+package fing.asignamax3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asignamax3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asignamax3000Application.class, args);
+	}
+
+}
